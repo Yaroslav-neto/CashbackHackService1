3f4dc3cfd2938d7f955389a3735cff33de6460b4
Yar neto <quatro30@gmail.com>
2025-04-27 16:15:08 +0700

    add test, change build

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,43 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainPay900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainPay1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainPay1001() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainPay1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        Assert.assertEquals(expected, actual);
+    }
+}
